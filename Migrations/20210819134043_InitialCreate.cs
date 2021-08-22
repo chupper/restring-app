00a7802ea09007cdeab0d3d88217dbe3b687d802
Karl Dowland <kdowland@hsdyn.com>
2021-08-22 20:37:32 +0100

    init

---
+++ Migrations/20210819134043_InitialCreate.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RestringApp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    ContactId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.ContactId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Restrings",
+                columns: table => new
+                {
+                    RestringId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RacketBrand = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RacketModel = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Tension = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ContactId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Restrings", x => x.RestringId);
+                    table.ForeignKey(
+                        name: "FK_Restrings_Contacts_ContactId",
+                        column: x => x.ContactId,
+                        principalTable: "Contacts",
+                        principalColumn: "ContactId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Restrings_ContactId",
+                table: "Restrings",
+                column: "ContactId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Restrings");
+
+            migrationBuilder.DropTable(
+                name: "Contacts");
+        }
+    }
+}
